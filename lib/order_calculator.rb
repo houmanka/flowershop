fb2bbbccdf18daaad01563c14234422f00024645
Houman kargaran <halfman58@yahoo.com>
2017-06-25 22:34:57 +1000

    Order calculator skeleton is added

---
+++ lib/order_calculator.rb
@@ -0,0 +1,55 @@
+# @author Houman Kargaran - 2017
+# This module acts as a Class Module for the order. It does the calculation
+
+module OrderCalculator
+
+    # Using the single pack and the user full order to put together the single order result
+    #
+    # @param
+    # @return
+
+    def calculate(single_pack, order)
+
+        result = []
+
+
+
+        return result
+    end
+
+    # (see #calculate)
+    # note: don't need Test. This is just the refactor after the fact
+
+    def package_formatter(best_price_combo, order_code_and_quantity, result, single_pack)
+
+    end
+
+
+
+    # This will calculate the package combinations, all of the possibilities.
+    #
+    # @param
+    # @return
+    # @reference:
+
+    def calculate_package_combinations(packs, target_number, combination = [], bucket = [])
+
+
+
+        return combination
+    end
+
+    # Calculating the price
+    #
+    # @param
+    # @return:
+
+    def calculate_price_for_best_combination( combination, single_pack)
+
+
+
+        return  { :total => [], :best_combination => combination }
+
+    end
+
+end
