3eca9687fa87e62180c15f137dec26d8dce58028
Houman kargaran <halfman58@yahoo.com>
2017-06-25 21:53:37 +1000

    Language file is added

---
+++ lib/language.rb
@@ -0,0 +1,7 @@
+ORDER_HEADER = 'Your order please: '
+ORDER_EXAMPLE = 'Example order: 10 T58'
+ORDER_COMPLETE = 'Type done and enter to calculate.'
+ORDER_FORMAT_ERROR = 'Please pay attention to the item code format and qty'
+THANK_YOU_MESSAGE = 'Thank you for your order. Calculating now... '
+CONTINUE = 'Recorded. Enter your new order or done to calculate: '
+DONE = 'done'
