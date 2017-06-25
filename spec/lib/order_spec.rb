8403da5bab5ed4d649050d437bf385b3d3790ee6
Houman kargaran <halfman58@yahoo.com>
2017-06-25 22:05:49 +1000

    Order is added

---
+++ spec/lib/order_spec.rb
@@ -0,0 +1,37 @@
+require 'order'
+require 'inventory'
+require 'support/let_singleton'
+
+
+describe Order do
+
+
+    context 'Save the order' do
+
+        it 'needs to record the order in an array' do
+
+        end
+
+    end
+
+    it 'expects to extract each order' do
+    end
+
+    it 'expects the inventory via customer order to match the sample output' do
+
+    end
+
+    it 'expects to process customer order' do
+
+    end
+
+    describe 'Formatter' do
+
+        # this behavior needs to be tested at the application. As it is the user output
+        it 'expect to return String' do
+        end
+
+    end
+
+
+end
