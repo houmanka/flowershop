f2e0c5883411fbaaff824b4e62789b081370767a
Houman kargaran <halfman58@yahoo.com>
2017-06-25 22:28:40 +1000

    Order Worker spec update

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,9 @@
 require 'inventory'
 
 class Order
+
+    extend OrderWorker
+
     def initialize
         @customer_order = []
         @an_order_item = []
@@ -12,7 +15,9 @@ class Order
     # @return [Boolean]
 
     def has_been_recorded?(order)
-
+        unless Validation.validate(order.to_s) && Order.is_a_valid_item_code?(order.to_s)
+            return false
+        end
         @customer_order.push order.to_s
         return  true
     end
@@ -26,4 +31,28 @@ class Order
     def customer_order
         @customer_order
     end
+
+
+    # Output of Order Class to user
+    #
+    # @param
+    # @return [String]
+
+    def fulfill_order
+        return ''
+    end
+
+    # collection of the User Order
+    #
+    # @param
+    # @return [Array<Hash>]
+    #   sample - [ { total: 17.98, sku_number: 'VS5', qty: 14 }, [ { qty: 1, pack: 5, each_pack: 8.99}, { qty: 3, pack: 3, each_pack: 8.99}]]
+
+    def process_customer_order
+
+
+
+        return []
+    end
+
 end
