6b50c292ab590baf690f42c864b35602905233d6
Houman kargaran <halfman58@yahoo.com>
2017-06-25 22:40:28 +1000

    Order Worker test passes - Not Stable

--- spec/lib/order_worker_spec.rb
+++ spec/lib/order_worker_spec.rb
@@ -1,57 +1,58 @@
 require 'order'
+require 'order_worker'
+require 'rose'
+require 'tulip'
+require 'lilie'
 require 'validation'
-require 'inventory'
 require 'support/let_singleton'
 
-describe Order do
 
-    let(:order) {
-        Order.new
-    }
+describe OrderWorker do
+
+    # initialise it so when we go to order it acts the same way
+    class Order
+    end
+
+    before(:each) do
+        @order_class = Order.new
+        @order_class.extend(OrderWorker)
+    end
 
     let (:singleton) {
         LetSingleton.instance
     }
 
-    before(:example) {
-        singleton.sample_orders[:valid_orders].each do |item|
-            order.has_been_recorded?(item)
-        end
-        @customer_order = order.customer_order
+
+    let (:single_pack) {
+        @order_class.fetch_from_inventory(singleton.sample_orders[:valid_orders].first)
     }
 
-    context 'Save the order' do
+    let (:valid_order) {
+        singleton.sample_orders[:valid_orders].first
+    }
 
-        it 'needs to record the order in an array' do
-            valid_orders = singleton.sample_orders[:valid_orders]
-            expect(order.customer_order).to match_array(valid_orders)
+
+
+    describe 'Save the order' do
+
+        def is_a_valid_helper(item_codes)
+            @order_class.is_a_valid_item_code?(item_codes)
+        end
+
+        it 'expects to check if item code is a valid code' do
+            expect(is_a_valid_helper(valid_order)).to be_truthy
+        end
+
+        it 'expects to return false if the item code is invalid' do
+            expect(is_a_valid_helper(singleton.sample_orders[:invalid_orders].first)).to be_falsy
         end
 
     end
 
-    it 'expects to extract each order' do
-        expect(@customer_order).not_to be_empty
-    end
+    describe 'Worker' do
 
-    it 'expects the inventory via customer order to match the sample output' do
-        expect(Order.fetch_from_inventory(@customer_order[0])).to satisfy {
-            |x| x[0].include?(:pack)
-        }
-    end
-
-    it 'expects to process customer order' do
-        customer_order = order.process_customer_order[0]
-        expected = customer_order.select { |x| x.first }
-        expect( expected ).to satisfy{
-            |x| x[0].keys.include?(:summery)
-        }
-    end
-
-    describe 'Formatter' do
-
-        # this behavior needs to be tested at the application. As it is the user output
-        it 'expect to return String' do
-            expect(order.fulfill_order).to be_instance_of(Array)
+        it 'expects to extract the qty and item code in Hash' do
+            expect(@order_class.extract_code_qty(valid_order)).to include(:qty, :code)
         end
 
     end
